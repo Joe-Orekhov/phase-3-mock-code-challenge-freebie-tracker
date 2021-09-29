22c1e5b93791dcb10a0f49ca8223ca7a473357e5
joesephorekhov <josephorekhov@gmail.com>
2021-09-29 16:31:00 -0600

    commit tree 936f01ebe66efdfb2b2f1f6bcf2068de557c0f10

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
-
+  has_may :freebies
+  has_many :companies, through: :freebies
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,4 @@
 class Dev < ActiveRecord::Base
-
+  has_may :freebies
+  has_many :devs, through: :freebies
 end
